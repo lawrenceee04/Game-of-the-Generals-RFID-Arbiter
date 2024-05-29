f5f2c26826f7b0b8d93dc106cd4a8e5feec72ce6
Lawrence Garcia <lawrencelg04@gmail.com>
2024-05-29 20:26:05 +0800

    removed unnecessary lines of code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         9
-#define SS_PIN          10
-#define NO_OF_PIECES    21
-#define RED_RGB_LED     6
-#define GREEN_RGB_LED   5
-#define BLUE_RGB_LED    3
-#define RESET_PIECES    2
+#define RST_PIN         D3        // Configurable, see typical pin layout above
+#define SS_PIN          D8        // Configurable, see typical pin layout above
+#define NO_OF_PIECES    21        
+// #define RESET_BUTTON    2
+#define RED_RGB_LED     6         // Configurable, see typical pin layout above
+#define GREEN_RGB_LED   5         // Configurable, see typical pin layout above
+#define BLUE_RGB_LED    3         // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
@@ -132,11 +132,7 @@ void setup() {
   Serial.begin(9600); // Initialize serial communication at 9600 baud
   SPI.begin();        // Init SPI bus
   mfrc522.PCD_Init(); // Init MFRC522 card
-  // pinMode(RESET_PIECES, INPUT);
-  // attachInterrupt(digitalPinToInterrupt(RESET_PIECES), ISR_Button, RISING); // attach interrupt
 }
-
-volatile bool resetFlag = false; // Add this line at the top of your file
 
 void loop() {
     // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
@@ -159,18 +155,18 @@ void loop() {
   for (int i = 0; i < NO_OF_PIECES; i++) {
     if (memcmp(mfrc522.uid.uidByte, whitePiece[i], 4) == 0) {
       whitePieceRank = pieceRanks[i];
-	  firstScannedPiece = 1;
+      firstScannedPiece = 1;
       break;
     } else if (memcmp(mfrc522.uid.uidByte, blackPiece[i], 4) == 0) {
-	  blackPieceRank = pieceRanks[i];
-	  firstScannedPiece = 2;
-	  break;
+      blackPieceRank = pieceRanks[i];
+      firstScannedPiece = 2;
+      break;
 	  }
   }
 
   // Wait for a second card
   while ( ! mfrc522.PICC_IsNewCardPresent()) {
-    delay(1000);
+    delay(20);
   }
 
   // Select the second card
@@ -185,12 +181,12 @@ void loop() {
   for (int i = 0; i < NO_OF_PIECES; i++) {
     if (memcmp(mfrc522.uid.uidByte, whitePiece[i], 4) == 0) {
       whitePieceRank = pieceRanks[i];
-	  firstScannedPiece = 1;
+      firstScannedPiece = 1;
       break;
     } else if (memcmp(mfrc522.uid.uidByte, blackPiece[i], 4) == 0) {
-	  blackPieceRank = pieceRanks[i];
-	  firstScannedPiece = 2;
-	  break;
+      blackPieceRank = pieceRanks[i];
+      firstScannedPiece = 2;
+      break;
 	  }
   }
 
@@ -201,11 +197,11 @@ void loop() {
   } else if (blackPieceRank == SPY && whitePieceRank == PRIVATE) {
     Serial.println("WHITE piece wins.");
     ledWinnerIndicator(1);
-// Flag is captured
+  // If a flag is captured
   } else if (firstScannedPiece == 2 && whitePieceRank == FLAG && blackPieceRank == FLAG) {
     Serial.println("WHITE piece wins, black's flag is captured.");
     ledWinnerIndicator(2);
-  } else if (firstScannedPiece == 1 && blackPieceRank == FLAG && whitePieceRank == FLAG) {
+  } else if (firstScannedPiece == 1 && whitePieceRank == FLAG && blackPieceRank == FLAG) {
     Serial.println("BLACK piece wins, white's flag is captured.");
     ledWinnerIndicator(1);
   } else if (whitePieceRank > blackPieceRank) {
